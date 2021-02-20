4213677c4d29f0bc292462e25e70c427f702cdbb
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-20 17:35:31 +0200

    move sneak

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 from turtle import Screen, Turtle
+import time
 
 #screen details
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Sneky Snek')
+screen.tracer(0)
 
 #create snek initial body
 
-x_cor = -20 #x coordinate
-y_cor = 0 #y coordinate
+starting_position = [(0,0), (-20,0),(-40,0)]
 
-for i in range(3):
-    sneak_segment = Turtle()
+sneak_segments = []
+
+for p in starting_position:
+    sneak_segment = Turtle("square")
     sneak_segment.penup()
     sneak_segment.color('white')
-    sneak_segment.shape('square')
-    sneak_segment.goto(x_cor,y_cor)
-    x_cor +=20
+    sneak_segment.goto(p)
+    sneak_segments.append(sneak_segment)
 
+
+game_is_on = True
+
+#move sneak
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(sneak_segments)-1, 0,-1):
+        new_x = sneak_segments[seg_num-1].xcor()
+        new_y = sneak_segments[seg_num-1].ycor()
+
+    sneak_segments[0].forward(20)
 
 
 
