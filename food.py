3875558134b270ccc16b9c0103536fb333a0f24a
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-20 19:53:37 +0200

    made refresh method

--- food.py
+++ food.py
@@ -12,3 +12,8 @@ class Food(Turtle):
         random_x = random.randint(-280,280)
         random_y = random.randint(-280,280)
         self.goto(random_x,random_y)
+    
+    def refresh():
+        random_x = random.randint(-280,280)
+        random_y = random.randint(-280,280)
+        self.goto(random_x,random_y)
