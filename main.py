70cf2a2a25658f2baab1d16b38f2e27e46d867f1
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-20 17:07:17 +0200

    sneak initial body

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Sneky Snek')
 
+#create snek initial body
 
+x_cor = -20 #x coordinate
+y_cor = 0 #y coordinate
 
-
+for i in range(3):
+    sneak_segment = Turtle()
+    sneak_segment.penup()
+    sneak_segment.color('white')
+    sneak_segment.shape('square')
+    sneak_segment.goto(x_cor,y_cor)
+    x_cor +=20
 
 
 
