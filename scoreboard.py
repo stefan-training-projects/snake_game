e1e17bef63958ebe0371c187c398b5ca3b3f492b
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-20 20:45:14 +0200

    add game_over method

--- scoreboard.py
+++ scoreboard.py
@@ -12,6 +12,10 @@ class Scoreboard(Turtle):
         self.score = 0
         self.write(f"Score: {self.score} ",font=FONT)
 
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER.", font=FONT)
+
     def update_score(self):
         self.clear()
         self.score += 1
