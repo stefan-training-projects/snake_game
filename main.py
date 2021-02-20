4241dbe8f62b44565eb16914e222562e490f5c9a
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-20 20:25:00 +0200

    added score to screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 #screen details
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+score = Scoreboard()
 
 
 #snake key commands
@@ -32,6 +34,7 @@ while game_is_on:
     #Detect collision between snake and food
     if snake.snake_head.distance(food) < 15:
         food.refresh()
+        score.update_score()
 
 
 
