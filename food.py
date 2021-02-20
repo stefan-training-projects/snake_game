3a005a62bc071c09a48ff43df4a67e1f2d1a9f0f
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-20 19:58:40 +0200

    fixed refresh method (added self)

--- food.py
+++ food.py
@@ -11,7 +11,7 @@ class Food(Turtle):
         self.speed("fastest")
         self.refresh()
 
-    def refresh():
+    def refresh(self):
         random_x = random.randint(-280,280)
         random_y = random.randint(-280,280)
         self.goto(random_x,random_y)
