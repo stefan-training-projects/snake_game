5f7e2b8b5492fa660e18cf83991b590e535cdff9
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-20 19:44:27 +0200

    created Food Class

--- food.py
+++ food.py
@@ -1,1 +1,14 @@
 from turtle import Turtle
+import random
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('cirle')
+        self.penup()
+        self.shapesize(stretch_len=0.5,stretch_wid=0.5)
+        self.color('purple')
+        self.speed("fastest")
+        random_x = random.randint(-280,280)
+        random_y = random.randint(-280,280)
+        self.goto(random_x,random_y)
