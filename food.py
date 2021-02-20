ebfcf4905ba7cec5fcd8cc0db0e032f4e18d6e8d
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-20 19:54:57 +0200

    added refresh to init

--- food.py
+++ food.py
@@ -9,10 +9,8 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5,stretch_wid=0.5)
         self.color('purple')
         self.speed("fastest")
-        random_x = random.randint(-280,280)
-        random_y = random.randint(-280,280)
-        self.goto(random_x,random_y)
-    
+        self.refresh
+
     def refresh():
         random_x = random.randint(-280,280)
         random_y = random.randint(-280,280)
