ddb2633d12ce20e023a3a9836f13b15abbb3250e
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-20 19:51:22 +0200

    fixed typo sneak to snake

--- snake.py
+++ snake.py
@@ -11,9 +11,9 @@ RIGHT = 0
 class Snake():
 
     def __init__(self):
-        self.sneak_segments = []
+        self.snake_segments = []
         self.create_snake()
-        self.sneak_head = self.sneak_segments[0]
+        self.snake_head = self.snake_segments[0]
 
     def create_snake(self):
         for p in STARTING_POSITIONS:
@@ -21,29 +21,29 @@ class Snake():
             sneak_segment.penup()
             sneak_segment.color('white')
             sneak_segment.goto(p)
-            self.sneak_segments.append(sneak_segment)
+            self.snake_segments.append(sneak_segment)
 
     def move(self):
 
-        for seg_num in range(len(self.sneak_segments)-1, 0,-1):
-            new_x = self.sneak_segments[seg_num-1].xcor()
-            new_y = self.sneak_segments[seg_num-1].ycor()
-            self.sneak_segments[seg_num].goto(new_x,new_y)
+        for seg_num in range(len(self.snake_segments)-1, 0,-1):
+            new_x = self.snake_segments[seg_num-1].xcor()
+            new_y = self.snake_segments[seg_num-1].ycor()
+            self.snake_segments[seg_num].goto(new_x,new_y)
 
-        self.sneak_head.forward(MOVE_DISTANCE)
+        self.snake_head.forward(MOVE_DISTANCE)
 
     def up(self):
-        if self.sneak_head.heading() != DOWN:
-            self.sneak_head.setheading(UP)
+        if self.snake_head.heading() != DOWN:
+            self.snake_head.setheading(UP)
 
     def down(self):
-        if self.sneak_head.heading() != UP:
-            self.sneak_head.setheading(DOWN)
+        if self.snake_head.heading() != UP:
+            self.snake_head.setheading(DOWN)
 
     def left(self):
-        if self.sneak_head.heading() != RIGHT:
-            self.sneak_head.setheading(LEFT)
+        if self.snake_head.heading() != RIGHT:
+            self.snake_head.setheading(LEFT)
 
     def right(self):
-         if self.sneak_head.heading() != LEFT:
-            self.sneak_head.setheading(RIGHT)
+         if self.snake_head.heading() != LEFT:
+            self.snake_head.setheading(RIGHT)
