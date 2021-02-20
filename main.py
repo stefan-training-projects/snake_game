973bffbd547ca67a3f68c103e40e75418306d65b
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-20 16:56:56 +0200

    screen setup

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import Screen, Turtle
+
+#screen details
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('Sneky Snek')
+
+
+
+
+
+
+
+
+screen.exitonclick()
