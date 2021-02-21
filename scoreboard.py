37a08a05e908f2f6da62f9313828e17a510ddfb7
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-21 22:51:51 +0200

    increase score method

--- scoreboard.py
+++ scoreboard.py
@@ -12,13 +12,11 @@ class Scoreboard(Turtle):
         self.color('white')
         self.hideturtle()
         self.penup()
-        self.goto(-40,280)
+        self.goto(-80,280)
         self.update_score()
 
     def update_score(self):
         self.clear()
-        self.score += 1
-        self.goto(-80,280)
         self.write(f"Score: {self.score} High Score: {self.high_score} ",font=FONT)
 
     def reset(self):
@@ -26,5 +24,10 @@ class Scoreboard(Turtle):
             self.high_score = self.score
         self.score = 0
         self.update_score()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
+
 
 
